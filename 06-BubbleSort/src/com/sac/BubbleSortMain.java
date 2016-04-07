30a445da834f4ce132697556159b99e412c8140b
S R Naik <srnaik@hotmail.com>
2016-04-06 21:34:31 -0500

    Bubble sort algorithm in Java

---
+++ 06-BubbleSort/src/com/sac/BubbleSortMain.java
@@ -0,0 +1,18 @@
+package com.sac;
+
+public class BubbleSortMain {
+
+	public static void main(String[] args) {
+	
+		int[] arrayElements = {12,10,8,6,4,14,18,16,24,2};
+		
+		BubbleSort bubbleSort = new BubbleSort();
+		arrayElements = bubbleSort.sortArrayElements(arrayElements);
+
+		System.out.println("Sorted Array ");
+		for (int i : arrayElements) {
+			System.out.println(i);
+		}
+	}
+
+}
